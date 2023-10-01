0c232db34a5b1de57bae297b2a1530c0b27fe154
Rahil Rafiq <146660442+RafiqRahil@users.noreply.github.com>
2023-10-01 23:08:12 +0530

    Add files via upload

---
+++ 1bhelloworldnewline.c
@@ -0,0 +1,10 @@
+//Title of Program
+//Program to print Hello world using newline character
+//Header Files
+#include <stdio.h>
+//Function Heading
+void main()
+{
+    //Output Statement
+    printf("Hello World\nHello World");
+}
